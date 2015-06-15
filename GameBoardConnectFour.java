63c26279e96f0b7cf957fed3645a1229d3297f46
Farco91 <corentin.farrugia@gmail.com>
2015-06-15 23:09:09 +0200

    Suppression des fichiers inutiles

--- GameBoardConnectFour.java
+++
@@ -1,5 +0,0 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("023a9b01-deab-4d77-9b42-fffcc9714167")
-public class GameBoardConnectFour extends GameBoard {
-}
--- GameBoardReversi.java
+++
@@ -1,5 +0,0 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("b7098db2-f788-4ae4-8b1c-8c0ef97bdda0")
-public class GameBoardReversi extends GameBoard {
-}
